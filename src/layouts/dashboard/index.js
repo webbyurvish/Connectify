2d15ab4009553d08147149f59ee644665df4fbbf
Urvish <webbyurvish@gmail.com>
2024-06-06 23:27:52 +0530

    Disabled auth is true

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -2,21 +2,21 @@ import { Navigate, Outlet } from "react-router-dom";
 import { Stack } from '@mui/material';
 import SideBar from "./SideBar";
 
-const isAuthenticated = true;
+const isAuthenticated = false;
 
 const DashboardLayout = () => {
 
-if(!isAuthenticated){
-  return <Navigate to='/auth/login'/>;
-}
+  if (!isAuthenticated) {
+    return <Navigate to='/auth/login' />;
+  }
 
   return (
     <Stack direction='row'>
       {/* SideBar */}
-      <SideBar/>
+      <SideBar />
       <Outlet />
     </Stack>
-    
+
   );
 };
 
